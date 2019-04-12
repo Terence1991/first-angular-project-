ec6bb85458bac293048aed2a6c4ab3d6e5675950
terence1991 <cplterencehawes@gmail.com>
2019-04-12 11:46:50 -0400

    server name was created dynamically

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -21,7 +21,7 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
-    this.serverCreationStatus = 'server was created'
+    this.serverCreationStatus = `Server was created. The name of the server is ${this.serverName}`
   }
 
   onUpdateServerName(event: any) {
